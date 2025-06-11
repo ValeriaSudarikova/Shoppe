3b453246699ca82429fcdb4d8d372a53ba539f07
ValeriaSudarikova <157243845+ValeriaSudarikova@users.noreply.github.com>
2025-06-11 08:03:35 +0300

    feat: add media of header-slider (#6)

--- components/HeaderSliderImgContent.vue
+++ components/HeaderSliderImgContent.vue
@@ -19,20 +19,44 @@
     padding: 20px 40px;
     color: white;
 
+    @media(max-width: $breakpoints-l-m) {
+        justify-content: flex-end;
+        padding: 90px 40px;
+    }
+
+    @media(max-width: $breakpoints-s) {
+        padding: 26px 8px;
+    }
+
     &-heading {
         @extend %h1;
         color: #fff;
+
+        @media(max-width: $breakpoints-m) {
+            @include h4(#fff)
+        }
     }
 
     &-price {
         @extend %h2;
         margin: 16px 0 48px;
         color: #fff;
+
+        @media(max-width: $breakpoints-m) {
+            @include t-medium(#fff);
+            margin: 4px 0 10px;
+        }
     }
 
     &-btn {
         @include button-empty(2px solid #fff, 6px, #fff);
         max-width: 194px;
+
+        @media(max-width: $breakpoints-m) {
+            @include t-small(#fff);
+            max-width: 92px;
+            height: 32px;
+        }
     }
 }
 </style>
