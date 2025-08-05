c391ec2897dc6022f4d1edfcf6d534a39da8a719
ValeriaSudarikova <lerbel@list.ru>
2025-08-05 17:34:37 +0300

    fix: update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-    compatibilityDate: '2024-11-01',
-    devtools: { enabled: true },
-    modules: [
-        '@nuxt/eslint',
-        '@nuxt/image',
-        '@pinia/nuxt',
-        '@primevue/nuxt-module',
-    ],
-    primevue: {
-        components: {
-            include: ['Slider'],
-        },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/image', '@pinia/nuxt', '@primevue/nuxt-module'],
+  primevue: {
+    components: {
+      include: ['Slider'],
     },
-    css: ['@/assets/scss/style.scss'],
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: `@use "@/assets/scss/variables.scss" as *;`,
-                },
-            },
+  },
+  css: ['@/assets/scss/style.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/scss/variables.scss" as *;`,
         },
+      },
     },
-});
+  },
+})
