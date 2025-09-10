fb830fabb6ae3e96fbabdbfc9b897a2a047735ad
Иван Баринов <i.barinov@itlabrealty.ru>
2025-09-10 21:44:40 +0300

    fix: test fix 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@use "~/assets/scss/variables.scss" as *;`,
+          additionalData: `@import "~/assets/scss/variables.scss";`,
         },
       },
     },
